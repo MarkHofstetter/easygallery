8e751fe98d5f4eb9deeb61c6a3215064aca216c0
Thomas Holtkötter <thomas.holtkoetter@sinnerschrader.com>
2013-02-05 21:34:35 +0100

    finished ant build task

--- index.php
+++ index.php
@@ -13,20 +13,21 @@ $googleanalyticsid = "";
 <html>
 	<head>
 		<title><?php echo $title; ?></title>
-		<meta name="description" content="">
 		<!-- TODO add meta tags -->
+		<meta name="description" content="">
+		<meta name="version" content=""/>
 
 		<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
 		
-		<!-- TODO minify css -->
 		<link rel="stylesheet" href="easygallery/css/easygallery.css" type="text/css" media="screen" />
 		<link rel="stylesheet" href="easygallery/css/ext/jquery.fancybox.css?v=2.1.4" type="text/css" media="screen" />
 
-		<!-- TODO minify js -->
-		<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+		<!-- #dev head start -->
+		<script type="text/javascript" src="easygallery/js/ext/jquery-1.9.1.min.js"></script>
 		<script type="text/javascript" src="easygallery/js/ext/jquery.mousewheel-3.0.6.pack.js"></script>
-		<script type="text/javascript" src="easygallery/js/ext/jquery.fancybox.pack.js?v=2.1.4"></script>
-		<script type="text/javascript" src="easygallery/js/ext/jquery.fancybox-media.js?v=1.0.5"></script>
+		<script type="text/javascript" src="easygallery/js/ext/jquery.fancybox.pack.js"></script>
+		<script type="text/javascript" src="easygallery/js/ext/jquery.fancybox-media.js"></script>
+		<!-- #dev head end -->
 
 		<script type="text/javascript">
 			$(document).ready(function() {
