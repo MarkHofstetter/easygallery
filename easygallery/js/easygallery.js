6886c6c42156521a0832a5258047b650df381b9c
Thomas Holtkötter <thomas.holtkoetter@sinnerschrader.com>
2013-02-28 16:18:36 +0100

    minor fixes map

--- easygallery/js/easygallery.js
+++ easygallery/js/easygallery.js
@@ -13,7 +13,7 @@ function GalleryCtrl($scope, $http) {
 	    method: "GET"
 	}).success(function(data, status, headers, config) {
 	    $scope.previews = data.previews;
-	    document.getElementById("map_canvas").className = 'map hide';
+	    document.getElementById("map_canvas").className = 'hide';
 	}).error(function(data, status, headers, config) {
 	    $scope.status = status;
 	});
@@ -27,7 +27,7 @@ function ImageCtrl($scope, $http, $routeParams) {
 	    $scope.images = data.images;
 	    $scope.dir = data.dir;
 	    $scope.exifavailable = data.exifavailable;
-		document.getElementById("map_canvas").className = 'map hide';
+		document.getElementById("map_canvas").className = 'hide';
 	}).error(function(data, status, headers, config) {
 	    $scope.status = status;
 	});
@@ -40,7 +40,7 @@ function MapCtrl($scope, $http, $routeParams) {
 	}).success(function(data, status, headers, config) {
 	    $scope.images = data.images;
 	    $scope.dir = data.dir;
-	    document.getElementById("map_canvas").className = 'map show';
+	    document.getElementById("map_canvas").className = 'show';
 	    var map = initialize(data);
 	    map = setRoute(map, data.images);
 	    $scope.map = map;
@@ -50,7 +50,6 @@ function MapCtrl($scope, $http, $routeParams) {
 }
 
 function initialize() {
-	// Create an array of styles.
 	var styles = [{
 		stylers : [{
 			hue : "#00ffe6"
@@ -72,8 +71,6 @@ function initialize() {
 			visibility : "off"
 		}]
 	}];
-	// Create a new StyledMapType object, passing it the array of styles,
-	// as well as the name to be displayed on the map type control.
 	var styledMap = new google.maps.StyledMapType(styles, {
 		name : "Styled Map"
 	});
@@ -86,25 +83,24 @@ function initialize() {
 		}
 	};
 	var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-	//Associate the styled map with the MapTypeId and set it to display.
 	map.mapTypes.set('map_style', styledMap);
 	map.setMapTypeId('map_style');
 	return map;
 }
 
 function setRoute(map, images) {
-	// fill with data	
 	var thumbnails = [];
 	var locations = [];
+	var imageRefs = [];
 	var count = 0;
 	for (var i = 0; i < images.length; i++) {
 		if (isNotEmpty(images[i].exif)) {
 			thumbnails[count] = images[i].exif.thumbnail;
 			locations[count] = new google.maps.LatLng(images[i].exif.gps.lat, images[i].exif.gps.lng);
+			imageRefs[count] = images[i].img;
 			count++;
 		}
 	}
-	// Construct the polygon
 	var mypath = new google.maps.Polyline({
 		path : locations,
 		strokeColor : '#000088',
@@ -114,16 +110,17 @@ function setRoute(map, images) {
 		map : map
 	});
 
-	for (var i = 0; i < locations.length; i++) {	
+	for (var i = 0; i < count; i++) {	
 		var image = new google.maps.MarkerImage(thumbnails[i], new google.maps.Size(56, 56), new google.maps.Point(0, 0), new google.maps.Point(32, 52));
 		var marker = new google.maps.Marker({
 			position : locations[i],
 			map : map,
 			icon : image,
-			url : '//localhost/projects/SRC/'
+			url : imageRefs[i].src,
+			title: imageRefs[i].name
 		});
 		google.maps.event.addListener(marker, 'click', function() {
-			window.location.href = marker.url;
+			window.location.href = this.url;
 		});
 	}
 
